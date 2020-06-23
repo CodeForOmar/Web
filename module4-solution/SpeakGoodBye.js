2edc11048a47d204c60c11b748b4bdae26014bd0
CodeForOmar <66962065+CodeForOmar@users.noreply.github.com>
2020-06-23 19:48:55 +0600

    Add files via upload

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = function speak(name) {
+  console.log(speakWord + " " + name);
+}
+  window.byeSpeaker = byeSpeaker;
+})(window);
